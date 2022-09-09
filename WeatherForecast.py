c17945d1706f218dd3cb06a59016cb600b90ecfd
Jeanne Corbeau <jcorbeau@biomaneo.intra>
2022-09-09 16:11:55 +0200

    deleted useless file

--- WeatherForecast.py
+++
@@ -1,8 +0,0 @@
-from datetime import date, timedelta
-
-from Weather import Weather
-
-forecast = Weather("Paris")
-today = date.today()
-next_week = today + timedelta(7)
-print(forecast.precipitation_average(today, next_week))
